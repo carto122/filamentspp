645be71335a88f5b33d452c1c9cbcf5635c865ad
carto122 <carto1221006@gmail.com>
2025-04-22 19:35:14 +0700

    add feature form semua table

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function users() {
+        return $this->belogsTo(User::class, 'user_id');
+    }
+
+     public function departments() {
+        return $this->belogsTo(Department::class, 'department_id');
+    }
 }
